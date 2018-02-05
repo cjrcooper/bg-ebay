bc4ce4ed1dd4f609ae14a171b129c0665876ef07
Christopher Cooper <christopher.cooper@christopher.cooper.ansarada.lcl>
2018-02-06 08:38:20 +1100

    Configured the main search components for a small test

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const electron = require('electron')
 // Module to control application life.
 const app = electron.app
@@ -6,6 +8,9 @@ const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
+
+
+require('electron-reload')(__dirname);
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
--- renderer.js
+++ renderer.js
@@ -1,3 +1,49 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+var ebay = require('ebay-api');
+var config = require('./config.js')
+var $ = require('./lib/jquery-3.3.1.min.js')
+
+$('#search-button').on('click', () => {
+  var params = {
+    keywords: ["Canon", "Powershot"],
+      // add additional fields
+    outputSelector: ['AspectHistogram'],
+
+    paginationInput: {
+      entriesPerPage: 10
+    },
+
+    itemFilter: [
+      {name: 'FreeShippingOnly', value: true},
+      {name: 'MaxPrice', value: '150'}
+    ],
+
+    domainFilter: [
+      {name: 'domainName', value: 'Digital_Cameras'}
+    ]
+  };
+
+  ebay.xmlRequest({
+      serviceName: 'Finding',
+      opType: 'findItemsByKeywords',
+      params: params,
+      parser: ebay.parseResponseJson,    // (default)
+      devId: config.devId,
+      certId: config.certId,
+      appId: config.appId,
+      authToken: config.authToken,
+      sandbox: true
+    },
+    // gets all the items together in a merged array
+    function itemsCallback(error, itemsResponse) {
+      if (error) throw error;
+
+      var items = itemsResponse.searchResult.item;
+
+      console.log('Found', items.length, 'items');
+      
+      for (var i = 0; i < items.length; i++) {
+        console.log('- ' + items[i].title);
+      }  
+    }
+  );
+});
