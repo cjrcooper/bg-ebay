e666ed3118d0499d6f026e6a37ef8887ea57cfad
Christopher Cooper <cjrcooper@gmail.com>
2018-03-16 01:48:27 +1100

    Added in new information in the columns and have a basic excel sheet working

--- renderer.js
+++ renderer.js
@@ -2,6 +2,12 @@ var ebay = require('ebay-api');
 var config = require('./config.js')
 var _ = require('lodash');
 var $ = require('./lib/jquery-3.3.1.js')
+var shell = require('electron');
+var fs = require('fs');
+var xl = require('excel4node');
+
+
+var excelDataResults = [];
 
 
 var setKeyWords = () => {
@@ -33,6 +39,40 @@ var checkEmptySearch = () => {
 }
 
 
+var downloadExcel = (data) => {
+  var wb = new xl.Workbook();
+  var ws = wb.addWorksheet("EbayListings");
+  var iterate = 1;
+
+  _.forEach(excelDataResults, (value) => {
+    ws.cell(1,iterate).string(value[0])
+    ws.cell(2,iterate).string(value[1])
+    ws.cell(3,iterate).number(value[2])
+    ws.cell(4,iterate).number(value[3])
+    console.log("test");
+    iterate++;
+  })
+  wb.write('Excel.xlsx');
+  var path = "Excel.xlsx";
+  fs.open(path, 'r+', (err, fd) => {
+    if(err) {
+      console.log(err);
+    }
+  })
+  console.log(`${process.cwd()}/Excel.xlsx`)
+}
+
+
+
+$(document).on('click', 'a[href^="http"]', function(e) {
+    e.preventDefault();
+    shell.shell.openExternal(this.href);
+});
+
+$('#excel-download').on('click', () => {
+  downloadExcel(excelDataResults);
+})
+
 $('#search-button').on('click', () => {
 
   checkEmptySearch();
@@ -48,7 +88,9 @@ $('#search-button').on('click', () => {
     itemFilter: [
       {name: 'FreeShippingOnly', value: freeShippingOnly()},
       {name: 'MaxPrice', value: setMaxPrice()}
-    ]
+    ],
+
+    outputSelector: "SellerInfo"
   };
 
   try {
@@ -80,15 +122,30 @@ $('#search-button').on('click', () => {
 
         var items = itemsResponse.searchResult.item;
         var final = "";
+        resultList = [];
 
         for (var i = 0; i < items.length; i++) {
-          var title = '<td>' + items[i].title + '</td>';
-          var price = '<td>' + '$' + items[i].sellingStatus.currentPrice.amount.toString() + '</td>'
-          var shipping = '<td>' + '$' + items[i].shippingInfo.shippingServiceCost.amount.toString() + '</td>';
 
-          final += '<tr>' + title + price + shipping + '</tr>';
+          var sellerData = items[i].sellerInfo.sellerUserName;
+          var titleData = items[i].title;
+          var priceData = items[i].sellingStatus.currentPrice.amount;
+          var shippingData = items[i].shippingInfo.shippingServiceCost.amount;
+          var linkData = items[i].viewItemURL;
+
+
+          var seller = '<td>' + sellerData + '</td>';
+          var title = '<td>' + titleData + '</td>';
+          var price = '<td>' + '$' + priceData.toString() + '</td>'
+          var shipping = '<td>' + '$' + shippingData.toString() + '</td>';
+          var link = '<td><a href="' + linkData + '" target="_blank"><i class="fas fa-external-link-alt"></i></a></td>'
+
+          var excelDataRow = [sellerData, titleData, priceData, shippingData];
+          excelDataResults.push(excelDataRow)
+
+          final += '<tr>' + seller + title + price + shipping + link + '</tr>';
         }
         $('#table tbody').html(final)
+        console.log(excelDataResults);
       }
     );
   } catch (e) {
